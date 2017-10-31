0b454a531556f443213b3839da0a861da3f985e9
BiancaMaciel <bianca_maciel.c@hotmail.com>
2017-10-30 23:31:24 -0200

    revised accounts

--- mooc/accounts/forms.py
+++ mooc/accounts/forms.py
@@ -1,38 +1,36 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.contrib.auth.models import User
 
 
-class RegisterForm(forms.ModelForm):
+class RegisterForm(UserCreationForm):
 
-    password1 = forms.CharField(label='Senha', widget=forms.PasswordInput)
-    password2 = forms.CharField(
-        label='Confirmação de Senha', widget=forms.PasswordInput
-    )
+    email = forms.EmailField(label='E-mail')
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError('A confirmação não está correta')
-        return password2
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Já existe usuário com este E-mail')
+        return email
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
+        user.email = self.cleaned_data['email']
         if commit:
             user.save()
         return user
 
-    class Meta:
-        model = User
-        fields = ['username', 'email']
-
 
 class EditAccountForm(forms.ModelForm):
 
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        queryset = User.objects.filter(
+            email=email).exclude(pk=self.instance.pk)
+        if queryset.exists():
+            raise forms.ValidationError('Já existe usuário com este E-mail')
+        return email
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'name']
+        fields = ['username', 'email', 'first_name', 'last_name']
--- mooc/accounts/urls.py
+++ mooc/accounts/urls.py
@@ -1,23 +1,16 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-
- 	url(r'^$', 'mooc.accounts.views.dashboard', 
-         name='dashboard'),
-
+    url(r'^$', 'mooc.accounts.views.dashboard', 
+        name='dashboard'),
     url(r'^entrar/$', 'django.contrib.auth.views.login', 
         {'template_name': 'accounts/login.html'}, name='login'),
-
     url(r'^sair/$', 'django.contrib.auth.views.logout', 
         {'next_page': 'core:home'}, name='logout'),
-
-
-	url(r'^cadastre-se/$', 'mooc.accounts.views.register', 
-         name='register'),
-
-	url(r'^editar/$', 'mooc.accounts.views.edit', 
-         name='edit'),
-
-	url(r'^editar-senha/$', 'mooc.accounts.views.edit_password', 
-         name='edit_password'),
+    url(r'^cadastre-se/$', 'mooc.accounts.views.register', 
+        name='register'),
+    url(r'^editar/$', 'mooc.accounts.views.edit', 
+        name='edit'),
+    url(r'^editar-senha/$', 'mooc.accounts.views.edit_password', 
+        name='edit_password'),
 )
